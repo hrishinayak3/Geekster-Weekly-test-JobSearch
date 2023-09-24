234129b2c5a711ba60f0a2160953c0e118198af0
Hrishikesh <hrishinayak3@gmail.com>
2023-09-25 01:10:38 +0530

    Upload extended commit

---
+++ src/main/java/com/geekster/weeklytest/project/JobSearchPortal/Model/Job.java
@@ -0,0 +1,41 @@
+package com.geekster.weeklytest.project.JobSearchPortal.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "Jobs")
+public class Job {
+
+
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private String location;
+    @Min(value = 20000)
+    private Double salary;
+    @Email
+    private String companyEmail;
+
+    private String companyName;
+    private String employerName;
+    @Enumerated(EnumType.STRING)
+    private JobType jobType;
+
+    @NotNull(message = "Date cannot be null")
+    @Past(message = "Date must be in the past")
+    private LocalDate appliedDate = LocalDate.now();
+
+}
---
+++ src/test/java/com/geekster/weeklytest/project/JobSearchPortal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.weeklytest.project.JobSearchPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
